817efda9a30fbf570ae7507d5002ea6e52fd4ae9
김예지 <ilice0403@naver.com>
2024-04-23 16:20:26 +0900

    feat: application.yml, application-dev.yml, application-s3.yml 생성

---
+++ backend/spring/ain/src/main/java/com/ssafy/ain/AinApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.ain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AinApplication.class, args);
+	}
+
+}
