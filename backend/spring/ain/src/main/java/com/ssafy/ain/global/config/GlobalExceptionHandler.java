306c8f88d63e662195e619fdb660e31e260ab67e
김예지 <ilice0403@naver.com>
2024-04-23 16:38:20 +0900

    feat: GlobalExceptionHandler 클래스 생성 및 세팅

---
+++ backend/spring/ain/src/main/java/com/ssafy/ain/global/config/GlobalExceptionHandler.java
@@ -0,0 +1,50 @@
+package com.ssafy.ain.global.config;
+
+import com.ssafy.ain.global.dto.ApiResponse;
+import com.ssafy.ain.global.exception.ExistException;
+import com.ssafy.ain.global.exception.InvalidException;
+import com.ssafy.ain.global.exception.NoExistException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
+
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler
+    public ApiResponse<?> handleMethodArgumentTypeMismatchException(MethodArgumentTypeMismatchException e) {
+        String type = String.valueOf(e.getRequiredType());
+
+        return ApiResponse.error(BAD_REQUEST, type.substring(type.lastIndexOf('.') + 1) +
+                " 타입 " + e.getName() + " 입력 형식이 올바르지 않습니다! (입력값 : " + e.getValue() + ")");
+    }
+
+    @ExceptionHandler
+    public ApiResponse<?> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        FieldError fieldError = e.getBindingResult().getFieldError();
+
+        return ApiResponse.error(BAD_REQUEST, fieldError.getDefaultMessage() + " (입력값: " + fieldError.getRejectedValue() + ")");
+    }
+
+    @ExceptionHandler
+    public ApiResponse<?> handleExistException(ExistException e) {
+
+        return ApiResponse.error(e.getCode());
+    }
+
+    @ExceptionHandler
+    public ApiResponse<?> handleNoExistException(NoExistException e) {
+
+        return ApiResponse.error(e.getCode());
+    }
+
+    @ExceptionHandler
+    public ApiResponse<?> handleInvalidException(InvalidException e) {
+
+        return ApiResponse.error(e.getCode());
+    }
+}
