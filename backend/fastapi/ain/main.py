a912ac5db5e9b432a6de8465d177d6b1bd0358d7
김예지 <ilice0403@naver.com>
2024-04-24 09:49:14 +0900

    feat: í cors 설정

--- backend/fastapi/ain/main.py
+++ backend/fastapi/ain/main.py
@@ -1,7 +1,18 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
+# FastAPI 호출
 app = FastAPI()
 
+# cors 설정
+origins = ["http://localhost:3000"]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 async def root():
