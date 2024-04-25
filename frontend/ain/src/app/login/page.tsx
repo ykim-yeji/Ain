dbf09aef43fbae9198b793faf6d3c8b4957b4d07
백천주 <dieswing@naver.com>
2024-04-25 09:26:36 +0900

    design: Modify image width and height

--- frontend/ain/src/app/login/page.tsx
+++ frontend/ain/src/app/login/page.tsx
@@ -7,13 +7,13 @@ export default function Page() {
 
   return (
     <div className='text-center flex flex-col items-center'>
-      <img className='mt-4' src='/rainbow-heart.gif' width={170} height={170} />
+      <img className='mt-6' src='/rainbow-heart.gif' width='35%' height='30%' />
       <div className='text-2xl text-white mt-10'>
         <div>더 많은 기능을 이용하시려면</div>
         <div>로그인을 진행해 주세요</div>
       </div>
-      <button onClick={kakaoLogin} className='mt-8'>
-        <img src='/kakao.png' width={180} height={45} />
+      <button type='button' onClick={kakaoLogin} className='mt-8 flex justify-center'>
+        <img src='/kakao.png' width='50%' height='100%' />
       </button>
       {/* <div className='flex mb-10'>
         <img className='mr-12' src='/left-cloud.png' width={200} />
