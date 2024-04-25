3c5e28625cd2f0f70b5909d350f67245b8ef08a2
jojoo <csh990720@gmail.com>
2024-04-26 08:57:06 +0900

    design: Modify 이상형 입력 페이지 css 수정

--- frontend/ain/src/app/create/page.tsx
+++ frontend/ain/src/app/create/page.tsx
@@ -5,6 +5,7 @@ import { useEffect, useState } from "react"
 export default function CreatePage() {
   const [currentNum, setCurrentNum] = useState<number>(1)
 
+  const [genderInput, setGenderInput] = useState<number>(0)
   const [faceInput, setFaceInput] = useState<string>('')
   const [shapeInput, setShapeInput] = useState<string>('')
   const [ectInput, setEctInput] = useState<string>('')
@@ -12,18 +13,37 @@ export default function CreatePage() {
   const [showPrev, setShowPrev] = useState<boolean>(false)
   const [showNext, setShowNext] = useState<boolean>(false)
 
-  const movePrev = () => {
-    setCurrentNum(currentNum -1)
+  const [mergeInput, setMergeInput] = useState<string>('')
+
+  const unsure = () => {
+    if (currentNum == 2) {
+      setCurrentNum(3)
+      setFaceInput('')
+    }
+    else if (currentNum == 3) {
+      setCurrentNum(4)
+      setShapeInput('')
+    }
+    else if (currentNum == 4) {
+      setCurrentNum(5)
+      setEctInput('')
+    }
   }
 
-  const moveNext = () => {
-    setCurrentNum(currentNum +1)
+  const submitInput = () => {
+    if (genderInput !== 0) {
+      setMergeInput(faceInput + shapeInput + ectInput)
+      // api 로직 구현
+    }
+    else {
+      alert('성별을 선택해 주세요')
+    }
   }
 
   useEffect(() => {
     if (currentNum == 1) {
       setShowPrev(false)
-      setShowNext(true)
+      setShowNext(false)
     }
     else if (currentNum == 2) {
       setShowPrev(true)
@@ -35,6 +55,10 @@ export default function CreatePage() {
     }
     else if (currentNum == 4) {
       setShowPrev(true)
+      setShowNext(true)
+    }
+    else if (currentNum == 5) {
+      setShowPrev(true)
       setShowNext(false)
     }
   }, [currentNum])
@@ -45,66 +69,113 @@ export default function CreatePage() {
 
   return <div className="w-full h-full flex flex-col">
     {/* 현재 인덱스 표시 */}
-    <div className="w-full h-[10%] pb-4 flex items-end justify-center">
+    <div className="w-full h-[15%] flex items-center justify-center">
       <div  className={`w-3 h-3 mx-2 ${currentNum === 1 ? 'bg-white' : 'bg-[#D9D9D9]'} rounded-full shadow-md`}></div>
       <div  className={`w-3 h-3 mx-2 ${currentNum === 2 ? 'bg-white' : 'bg-[#D9D9D9]'} rounded-full shadow-md`}></div>
       <div  className={`w-3 h-3 mx-2 ${currentNum === 3 ? 'bg-white' : 'bg-[#D9D9D9]'} rounded-full shadow-md`}></div>
       <div  className={`w-3 h-3 mx-2 ${currentNum === 4 ? 'bg-white' : 'bg-[#D9D9D9]'} rounded-full shadow-md`}></div>
+      <div  className={`w-3 h-3 mx-2 ${currentNum === 5 ? 'bg-white' : 'bg-[#D9D9D9]'} rounded-full shadow-md`}></div>
     </div>
+
     {/* 입력칸 영역 */}
-    <div className="w-full h-[75%] flex flex-row items-center justify-center">
-      <div className="w-[15%] h-full flex flex-col items-center">
+    <div className="w-full h-[85%] flex flex-row items-start justify-center">
+
+      {/* 이전으로 넘기는 화살표 버튼 */}
+      {/* <div className="w-[15%] h-[90%] flex flex-col items-center">
         {showPrev && (
         <button className="w-5 m-auto"
         onClick={movePrev}><img src="./icon/angle_left.png" /></button>
         )}   
-      </div>
+      </div> */}
+      
+      {currentNum !== 5 ? (
+      <div className="w-[70%] h-[90%] bg-[#F0D5FA] rounded-xl shadow-lg  flex flex-col items-center">
+        <div className="w-[80%] h-6 mt-[10%]">
+          <button onClick={(event) => setCurrentNum(currentNum -1)}>
+            <img src="./icon/angle_left_purple.png" className="w-[10px]" />            
+          </button>
 
-      {currentNum !== 4 ? (
-      <div className="w-[70%] h-full bg-[#F0D5FA] rounded-xl shadow-lg  flex flex-col items-center">
+        </div>
         {/* 질문 */}
-        <div className="w-[80%] h-[25%] flex flex-col justify-center text-center text-md text-bold text-black">
+        <div className="w-[80%] h-[15%] flex flex-col justify-start text-center text-lg text-bold text-black">
         {currentNum === 1 && (
-          <p>당신의 아인은 <br />어떤 이목구비를 가졌나요?</p>
+          <p>아인의 성별은 <br />무엇인가요?</p>
         )}
         {currentNum === 2 && (
-          <p>어떤 얼굴형과 <br />헤어스타일을 가졌나요?</p>
+          <p>당신의 아인은 <br />어떤 이목구비를 가졌나요?</p>
         )}
         {currentNum === 3 && (
+          <p>어떤 얼굴형과 <br />헤어스타일을 가졌나요?</p>
+        )}
+        {currentNum === 4 && (
           <p>기타 생김새를 <br /> 입력해주세요</p>
         )}
         </div>   
-        {/* 입력받기     */}
+         {/* 이상형 성별 입력 받기 */}
+        {currentNum == 1 ? (
+        <div className="w-[80%] h-[60%] flex justify-center items-center bg-white rounded-xl px-4">
+          <button  className={`flex flex-col items-center py-2 border-r-2 border-dashed rounded-md 
+          ${genderInput === 1 ? 'bg-[#C5EEFF]' : 'hover:bg-[#C5EEFF]'}`}
+          onClick={(event)=> {setGenderInput(1), setCurrentNum(2)}}>
+            <img src="./icon/boy.png" className="w-[80%]"/>
+            <p className="mt-6">남성</p>
+          </button>
+          <button  className={`flex flex-col items-center py-2 rounded-md 
+          ${genderInput === 2 ? 'bg-[#FFD2F2]' : 'hover:bg-[#FFD2F2]'}`}
+          onClick={(event)=> {setGenderInput(2), setCurrentNum(2)}}>
+            <img src="./icon/girl.png" className="w-[80%]"/>   
+            <p className="mt-6">여성</p>         
+          </button>
+        </div>)
+        // 이상형 생김새 입력 받기
+        : (
         <div className="w-[80%] h-[50%] flex justify-center items-center bg-white rounded-xl">
-        {currentNum === 1 && (
+        {currentNum === 2 && (
           <textarea className="w-[90%] text-sm outline-none resize-none text-center" rows={10}
           value={faceInput} onChange={(event)=> setFaceInput(event.target.value)} placeholder='이목구비 입력 예시'/>
         )}
-        {currentNum === 2 && (
+        {currentNum === 3 && (
           <textarea className="w-[90%] text-sm outline-none resize-none text-center" rows={10}
           value={shapeInput} onChange={(event)=> setShapeInput(event.target.value)} placeholder='얼굴형, 헤어스타일 입력 예시'/>
         )}
-        {currentNum === 3 && (
+        {currentNum === 4 && (
           <textarea className="w-[90%] text-sm outline-none resize-none text-center" rows={10}
           value={ectInput} onChange={(event)=> setEctInput(event.target.value)} placeholder='기타 생김새 입력 예시'/>
         )}
-        </div>
-        <div className="w-[80%] h-[25%] flex items-center justify-center">
-          <button className="w-full h-[60px] bg-[#EAACFF] rounded-3xl shadow-md text-center text-white text-lg">
-            <p>잘 모르겠어요</p>
+        </div>)}
+
+        {/* 잘 모르겠어요 버튼 */}
+        {currentNum !== 1 && (
+        <div className="w-[80%] h-[25%] flex flex-col items-center justify-center">
+          <button className="w-[150px] h-[50px] bg-[#C776E3] rounded-2xl shadow-md text-center text-white text-lg hover:bg-[#AB42CF]"
+          onClick={(event) => setCurrentNum(currentNum + 1)}>
+            <p>입력 완료</p>
           </button>
-        </div>
+          <button className="text-xs mt-4 text-[#5F0F7A]" 
+          onClick={(event) => setCurrentNum(currentNum + 1)}>잘 모르겠어요..</button>
+        </div>)}
       </div>)
+
+      // currentNum이 5일때
       : (
-        <div className="w-[70%] h-full"></div>
+        <div className="w-[70%] h-full flex flex-col justify-start items-center">
+            <img src="./gif/question.gif" className="w-[70%]"/>
+            <div className="w-[90%] h-[200px] bg-[#F0D5FA] rounded-2xl flex flex-col items-center justify-evenly shadow-xl">
+              <p className="text-lg text-center text-black">아인이 당신을 <br />기다리고 있어요!</p>
+              <button className="w-[70%] h-14 bg-[#AB42CF] rounded-xl text-center text-white shadow-lg"
+              onClick={submitInput}>만나러 가기</button>                  
+            </div>
+        </div>
       )}
 
-      <div className="w-[15%] h-full flex flex-col items-center">
+      {/* 다음으로 넘기는 화살표 버튼 */}
+      {/* <div className="w-[15%] h-[90%] flex flex-col items-center">
         {showNext && (
         <button className="w-5 m-auto"
         onClick={moveNext}><img src="./icon/angle_right.png" /></button> 
         )}   
-      </div>
+      </div> */}
+      
     </div>
   </div>
 }
---
+++ frontend/ain/src/app/loading/page.tsx
@@ -0,0 +1,4 @@
+export default function LoadingPage() {
+    return <div>로딩 페이지</div>;
+  }
+  
