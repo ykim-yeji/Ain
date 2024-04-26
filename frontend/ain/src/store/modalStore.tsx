578cb1ecec5bdf6e4480bbeb87caa49745f8e41f
백천주 <dieswing@naver.com>
2024-04-26 17:42:58 +0900

    feat: Add overlay condition and Modify modal state (useModalStore)

--- frontend/ain/src/store/modalStore.tsx
+++ frontend/ain/src/store/modalStore.tsx
@@ -2,21 +2,50 @@ import { create } from 'zustand';
 import { createJSONStorage, persist } from 'zustand/middleware';
 
 interface StoreState {
+  headerDropDown: boolean;
+  setHeaderDropDown: () => void;
   nicknameModalState: boolean;
   setNicknameModalState: () => void;
+  testNum: number;
+  increaseTestNum: () => void;
+  navOverlay: boolean;
+  toggleNavOverlay: () => void;
 }
 
-const useModalStore = create(
-  persist<StoreState>(
-    (set, get) => ({
-      nicknameModalState: false,
-      setNicknameModalState: () => set((state) => ({ nicknameModalState: !state.nicknameModalState })),
-    }),
-    {
-      name: 'modal',
-      storage: createJSONStorage(() => localStorage),
-    }
-  )
-);
+// const useModalStore = create(
+//   persist<StoreState>(
+//     (set, get) => ({
+//       nicknameModalState: false,
+//       setNicknameModalState: () => set((state) => ({ nicknameModalState: !state.nicknameModalState })),
+//       testNum: 1,
+//       increaseTestNum: () => set((state) => ({ testNum: state.testNum + 1 })),
+//     }),
+//     {
+//       name: 'modal',
+//       storage: createJSONStorage(() => localStorage),
+//     }
+//   )
+// );
+const useModalStore = create<StoreState>((set, get) => ({
+  headerDropDown: false,
+  setHeaderDropDown: () =>
+    set((state) => ({
+      headerDropDown: !state.headerDropDown,
+    })),
+  nicknameModalState: false,
+  setNicknameModalState: () =>
+    set((state) => ({
+      nicknameModalState: !state.nicknameModalState,
+      headerDropDown: false,
+      navOverlay: !state.navOverlay,
+    })),
+  testNum: 1,
+  increaseTestNum: () => set((state) => ({ testNum: state.testNum + 1 })),
+  navOverlay: false,
+  toggleNavOverlay: () =>
+    set((state) => ({
+      navOverlay: !state.navOverlay,
+    })),
+}));
 
 export default useModalStore;
