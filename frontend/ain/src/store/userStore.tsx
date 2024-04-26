8cf24d422f8e898695cce28920e06a453c79b98a
백천주 <dieswing@naver.com>
2024-04-26 17:46:54 +0900

    feat: Modify codes in userStore

--- frontend/ain/src/store/userStore.tsx
+++ frontend/ain/src/store/userStore.tsx
@@ -4,6 +4,11 @@ import { createJSONStorage, persist } from 'zustand/middleware';
 interface StoreState {
   isLogin: boolean;
   setIsLogin: () => void;
+  accessToken: string;
+  setAccessToken: (token: string) => void;
+  refreshToken: string;
+  setRefreshToken: (refreshToken: string) => void;
+  memeberId: number;
 }
 
 const useUserStore = create(
@@ -11,6 +16,15 @@ const useUserStore = create(
     (set, get) => ({
       isLogin: false,
       setIsLogin: () => set((state) => ({ isLogin: !state.isLogin })),
+      accessToken: '',
+      // getAccessToken: () => get().accessToken,
+      setAccessToken: (token: string) => set({ accessToken: token }),
+      refreshToken: '',
+      setRefreshToken: (refreshToken: string) =>
+        set({
+          refreshToken: refreshToken,
+        }),
+      memeberId: 0,
     }),
     {
       name: 'user',
