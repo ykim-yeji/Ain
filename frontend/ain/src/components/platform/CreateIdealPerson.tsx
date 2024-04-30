ffbc3cbd829992ece09846ceb9cd1cfa3d1ede9a
정성현 <gkgktjdgusdl@naver.com>
2024-04-30 15:42:29 +0900

    feat: Add n-data 페이지 디자인

--- frontend/ain/src/components/platform/CreateIdealPerson.tsx
+++ frontend/ain/src/components/platform/CreateIdealPerson.tsx
@@ -6,12 +6,18 @@ import Link from 'next/link'; // Next.js의 Link 컴포넌트를 임포트합니
 export const CreateIdealPersonPage = () => {
   return (
     <div className="create-ideal-person-page" style={{ textAlign: 'center', padding: '50px' }}>
-      <h1>이상형을 생성하세요!</h1>
-      <p>나만의 이상형을 만들어 보세요. 다양한 조건과 특징으로 이상형을 설정할 수 있습니다.</p>
+      <h1 className="text-white text-2xl mb-5">생성된 아인이 없어요...</h1>
+      <div className="flex justify-center items-center mb-10">
+          <img src="./image/no-data2.png" className="w-[90%]" />
+      </div>
+      <p className="text-white text-xl mb-14">아인을 만든 후 <br />소중한 순간을 기록하세요!</p>
       <Link href="/create">
-        <button>
-          이상형 생성하기
-        </button>
+      <button
+        type='button'
+        className='mb-6 text-md px-16 py-2 rounded-full text-white text-lg shadow-lg bg-purple-600'
+      >
+        생성하기
+      </button>
       </Link>
     </div>
   );
