ea86fe0b3448aa410e37236e0a0cc5cca9868db6
정성현 <gkgktjdgusdl@naver.com>
2024-05-03 17:30:35 +0900

    design: Add 기타 카메라 화면 디자인 완료

--- frontend/ain/src/components/platform/CreateIdealPerson.tsx
+++ frontend/ain/src/components/platform/CreateIdealPerson.tsx
@@ -40,35 +40,26 @@ export const CreateIdealPersonPage = () => {
     <div className="create-ideal-person-page px-[50px] flex flex-col justify-center items-center w-full h-full" style={{ textAlign: 'center',  overflow: 'hidden' }}>
        <h1 className="text-white text-2xl mb-5" style={{ fontWeight: 'bold' }}>생성된 아인이 없어요...</h1>
       <div className="flex justify-center items-center mb-7">
-          <img src="./gif/no-data.gif" className="w-[90%] rounded-full" />
+          <img src="./gif/no_data.gif" className="w-[90%] rounded-full" />
       </div>
       <GradientText className="text-gray-300 text-xl mb-7" style={{ fontWeight: "bold" }}>
         아인을 만든 후 <br />소중한 순간을 기록하세요!
       </GradientText>
       <div className="flex justify-center items-center mb-7">
           <motion.img
-            src="./image/chevron-down2.png"
+            src="./image/chevron_down2.png"
             className="w-[20%]"
             variants={variants}
             animate="animate"
           />
         </div>
       <Link href="/create" passHref>
-        <div
-          className="w-[220px] h-[50px] relative mx-auto cursor-pointer" // mx-auto 및 cursor-pointer 추가
+        <button
+          className='w-[200px] h-12 mb-2 bg-[#AB42CF] rounded-full text-center text-white text-xl shadow-md'
+          style={{ color: 'white', fontWeight: 'bold' }} // 텍스트 스타일 조정
         >
-          <img
-            src="./image/purple-button.png"
-            className="w-full h-full" // 이미지를 컨테이너 크기에 맞춤
-            alt="생성하기"
-          />
-          <div
-            className="absolute w-full h-full flex items-center justify-center top-0 left-0 text-lg"// 텍스트를 이미지 중앙에 배치
-            style={{ color: 'white', fontWeight: 'bold' }} // 텍스트 스타일 조정
-          >
-            생성하기
-          </div>
-        </div>
+          생성하기
+        </button>
       </Link>
     </div>
   );
