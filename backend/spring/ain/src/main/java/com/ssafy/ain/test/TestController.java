41899891bc1892867b2d697f7d7ef93eb6c0af78
core <jinjs6@gmail.com>
2024-05-05 12:16:23 +0900

    test: 결과 확인을 위한 testController 파일 작성

---
+++ backend/spring/ain/src/main/java/com/ssafy/ain/test/TestController.java
@@ -0,0 +1,13 @@
+package com.ssafy.ain.test;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/test")
+    public String getTestResponse() {
+        return "ok";
+    }
+}
