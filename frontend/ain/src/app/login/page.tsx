6ef0f544e1892bc0003ac4b89dcfdf646b6261e2
백천주 <dieswing@naver.com>
2024-05-08 17:44:47 +0900

    feat: Add code in login page (server redirect)

--- frontend/ain/src/app/login/page.tsx
+++ frontend/ain/src/app/login/page.tsx
@@ -12,24 +12,29 @@ export default function Page() {
   const kakaoLogin = () => {
     // location.href = process.env.NEXT_PUBLIC_KAKAO_OAUTH_URL as string
 
-    location.href =
-      `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${client_id}&redirect_uri=${redirect_uri}` as string;
+    // location.href =
+    //   `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${client_id}&redirect_uri=${redirect_uri}` as string;
+
+    window.location.href = 'https://myain.co.kr/api/oauth2/authorization/kakao';
   };
 
   return (
     <div className='relative mt-[65px] mb-[68px] w-full h-full flex flex-col items-center justify-start'>
       <div className='w-[60%] h-[50%] flex items-center'>
-        <img src='./gif/rainbow_heart.gif'  />
+        <img src='./gif/rainbow_heart.gif' />
       </div>
       <div className='w-full h-[40%] flex flex-col justify-start items-center'>
-        <div className='text-lg text-center text-white leading-relaxed'>더 많은 기능을 이용하시려면 <br />로그인을 진행해 주세요</div>
+        <div className='text-lg text-center text-white leading-relaxed'>
+          더 많은 기능을 이용하시려면 <br />
+          로그인을 진행해 주세요
+        </div>
         <button type='button' onClick={kakaoLogin} className='mt-10 flex justify-center'>
           <img src='/logo/kakao.png' width='50%' height='100%' />
         </button>
-        </div>
+      </div>
       {/* <img src="./background/login_top.png" className='absolute top-[40%] left-0 w-[45%]' /> */}
-      <img src="./background/login_left.png" className='absolute bottom-0 left-0 w-[40%]' />
-      <img src="./background/login_right.png" className='absolute bottom-0 right-0 w-[40%]' />
+      <img src='./background/login_left.png' className='absolute bottom-0 left-0 w-[40%]' />
+      <img src='./background/login_right.png' className='absolute bottom-0 right-0 w-[40%]' />
       {/* <img src="./background/login_center.png" className='absolute bottom-[5%] right-0 w-[35%]' /> */}
       {/* <img src="./background/login_bottom.png" className='absolute bottom-0 left-[20%] w-[40%]' /> */}
     </div>
