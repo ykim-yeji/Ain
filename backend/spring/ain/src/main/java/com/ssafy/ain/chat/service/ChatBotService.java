443a3a2288b0ef42cc511c3e5194e34376ba6918
김예지 <ilice0403@naver.com>
2024-05-10 10:08:05 +0900

    fix: ChatBotService addIdealPersonChatBot() 이상형 챗봇 생성 메소드 추가

--- backend/spring/ain/src/main/java/com/ssafy/ain/chat/service/ChatBotService.java
+++ backend/spring/ain/src/main/java/com/ssafy/ain/chat/service/ChatBotService.java
@@ -1,4 +1,8 @@
 package com.ssafy.ain.chat.service;
 
+import com.ssafy.ain.chat.dto.ChatBotOpenFeignDTO.*;
+
 public interface ChatBotService {
+
+    AddIdealPersonChatBotResponse addIdealPersonChatBot();
 }
