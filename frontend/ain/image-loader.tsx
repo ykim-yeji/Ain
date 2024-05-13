f2aae3bdabbbdb4e2a50f7aa8f3bd7d92ae7dc28
백천주 <dieswing@naver.com>
2024-05-13 11:02:03 +0900

    feat: Modfiy image-loader tsx to test

--- frontend/ain/image-loader.tsx
+++ frontend/ain/image-loader.tsx
@@ -5,8 +5,8 @@ interface Props {
 }
 
 export default function imageLoader({ src, width, quality }: Props) {
-  //   const params = [`imwidth = ${width}`];
-  //   return `${src}?${params}`;
-  return `https://myain.co.kr/${src}?w=${width}`;
+  const params = [`imwidth = ${width}`];
+  return `${src}?${params}`;
+  //   return `https://myain.co.kr/${src}?w=${width}`;
   //   return `http://localhost:3000/${src}?w=${width}`;
 }
