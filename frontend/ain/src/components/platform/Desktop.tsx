358bf977809ee26bc3d227f3c4f729b05ccfef6f
백천주 <dieswing@naver.com>
2024-05-13 10:24:44 +0900

    feat: Modify api url process env name

--- frontend/ain/src/components/platform/Desktop.tsx
+++ frontend/ain/src/components/platform/Desktop.tsx
@@ -37,7 +37,7 @@ export const DesktopPage = () => {
   useEffect(() => {
     const fetchIdealPersonsCount = async () => {
       try {
-        const response = await fetch(`${process.env.NEXT_PUBLIC_AIN_SPRING_API_URL}/ideal-people/count`);
+        const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/ideal-people/count`);
         const data = await response.json();
         if (data.code === 200 && data.status === 'OK') {
           setIdealPersons(data.data.idealPeople);
@@ -62,15 +62,14 @@ export const DesktopPage = () => {
 
     const fetchIdealPersons = async () => {
       try {
-    
-        const response = await fetch(`${process.env.NEXT_PUBLIC_AIN_SPRING_API_URL}/ideal-people`, {
+        const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/ideal-people`, {
           headers: {
-            'Authorization': `Bearer eyJhbGciOiJIUzI1NiJ9.eyJjYXRlZ29yeSI6ImFjY2Vzc1Rva2VuIiwibWVtYmVySWQiOjUsImlhdCI6MTcxNTMwOTQ2MywiZXhwIjoxNzE1MzEzMDYzfQ.u1gSw2NT9aM8deXJgc-29rXlwOvGl7mkjJ7p3jbZdW8` // 헤더에 액세스 토큰 추가
-          }
+            Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJjYXRlZ29yeSI6ImFjY2Vzc1Rva2VuIiwibWVtYmVySWQiOjUsImlhdCI6MTcxNTMwOTQ2MywiZXhwIjoxNzE1MzEzMDYzfQ.u1gSw2NT9aM8deXJgc-29rXlwOvGl7mkjJ7p3jbZdW8`, // 헤더에 액세스 토큰 추가
+          },
         });
-    
+
         const data = await response.json();
-    
+
         if (data.code === 200 && data.status === 'OK') {
           setIdealPersons(data.data.idealPeople);
         }
@@ -78,9 +77,9 @@ export const DesktopPage = () => {
         console.error('이상형 정보 가져오기 실패:', error);
       }
     };
-    
+
     fetchIdealPersons();
-    }, [idealPersonCount]);
+  }, [idealPersonCount]);
 
   const selectIdealPersonImage = (idealPersonImage: string) => {
     setSelectedIdealPersonImage(idealPersonImage);
@@ -101,20 +100,30 @@ export const DesktopPage = () => {
   };
 
   if (idealPersonCount === 0) {
-    return <CreateIdealPersonPage/>;
+    return <CreateIdealPersonPage />;
   }
 
   if (showIntro) {
     return (
-      <div className="intro-page w-full h-full flex flex-col justify-center items-center" style={{ textAlign: 'center'}}>
-        <h1 className="text-white text-2xl mb-5" style={{ fontWeight: 'bold' }}>아인과 함께 사진을 찍어요!</h1>
-        <div className="flex justify-center items-center mb-7">
-            <img src="./gif/camera_start.gif" className="w-[60%]" />
+      <div
+        className='intro-page w-full h-full flex flex-col justify-center items-center'
+        style={{ textAlign: 'center' }}
+      >
+        <h1 className='text-white text-2xl mb-5' style={{ fontWeight: 'bold' }}>
+          아인과 함께 사진을 찍어요!
+        </h1>
+        <div className='flex justify-center items-center mb-7'>
+          <img src='./gif/camera_start.gif' className='w-[60%]' />
         </div>
-        <p className="text-gray-300 text-xl mb-4">아인과 멋진 사진을 남기고 싶다면, <br/>카메라 권한을 허용해 주세요!</p>
-        <button onClick={handleStartCamera} 
-          className='w-[200px] h-12 mb-2 bg-[#AB42CF] rounded-full text-center text-white text-xl shadow-md'>
-            카메라 시작
+        <p className='text-gray-300 text-xl mb-4'>
+          아인과 멋진 사진을 남기고 싶다면, <br />
+          카메라 권한을 허용해 주세요!
+        </p>
+        <button
+          onClick={handleStartCamera}
+          className='w-[200px] h-12 mb-2 bg-[#AB42CF] rounded-full text-center text-white text-xl shadow-md'
+        >
+          카메라 시작
         </button>
       </div>
     );
@@ -129,22 +138,21 @@ export const DesktopPage = () => {
     idealPersonThreadId: string;
   }
 
-   const totalPages = Math.ceil((idealPersons?.length ?? 0) / itemsPerPage);
+  const totalPages = Math.ceil((idealPersons?.length ?? 0) / itemsPerPage);
 
-   const handleNextClick = () => {
-     // 다음 페이지로 이동, 마지막  페이지에서는 첫 번째 페이지로
-     setCurrentPage((prevPage) => (prevPage + 1) % totalPages);
-   };
-   
-   const handlePrevClick = () => {
-     // 이전 페이지로 이동, 첫 번째 페이지에서는 마지막 페이지로
-     setCurrentPage((prevPage) => (prevPage - 1 + totalPages) % totalPages);
-   };
+  const handleNextClick = () => {
+    // 다음 페이지로 이동, 마지막  페이지에서는 첫 번째 페이지로
+    setCurrentPage((prevPage) => (prevPage + 1) % totalPages);
+  };
 
-  const currentPageIdealPersons = idealPersons?.slice(
-      currentPage * itemsPerPage,
-      (currentPage + 1) * itemsPerPage
-    ).map((idealPerson) => ({
+  const handlePrevClick = () => {
+    // 이전 페이지로 이동, 첫 번째 페이지에서는 마지막 페이지로
+    setCurrentPage((prevPage) => (prevPage - 1 + totalPages) % totalPages);
+  };
+
+  const currentPageIdealPersons = idealPersons
+    ?.slice(currentPage * itemsPerPage, (currentPage + 1) * itemsPerPage)
+    .map((idealPerson) => ({
       idealPersonImageUrl: idealPerson.idealPersonImageUrl,
       idealPersonNickname: idealPerson.idealPersonNickname,
     }));
@@ -157,103 +165,109 @@ export const DesktopPage = () => {
   };
 
   return (
-    <div className="flex flex-col justify-center items-center space-y-9" style={{ height: "calc(100vh)", overflowY: "auto" }}>
-      <div className="relative w-10/12">
+    <div
+      className='flex flex-col justify-center items-center space-y-9'
+      style={{ height: 'calc(100vh)', overflowY: 'auto' }}
+    >
+      <div className='relative w-10/12'>
         {image ? (
-          <img src={image} className="w-full" alt="Captured" /> // 캡처된 이미지 표시
+          <img src={image} className='w-full' alt='Captured' /> // 캡처된 이미지 표시
         ) : (
           <video ref={videoRef} autoPlay muted /> // 비디오 스트림 표시
         )}
         {isCameraOn && selectedIdealPersonImage && (
-          <div style={{ position: "absolute", right: "0", bottom: "0" }}>
+          <div style={{ position: 'absolute', right: '0', bottom: '0' }}>
             <img
               src={selectedIdealPersonImage}
-              className="w-auto h-auto pointer-events-none"
-              alt="Overlay"
-              style={{ width: "134px", height: "134px" }}
+              className='w-auto h-auto pointer-events-none'
+              alt='Overlay'
+              style={{ width: '134px', height: '134px' }}
             />
           </div>
         )}
       </div>
 
-      <div className='flex flex-row justify-center items-center space-x-5' style={{ width: 'calc(3 * 134px)', justifyContent: 'space-between' }}>
-          {/* 이전 버튼 */}
-          <button onClick={handlePrevClick}>
-            <img src="./icon/angle_left_white.png" alt="이전" className='w-6'/>
+      <div
+        className='flex flex-row justify-center items-center space-x-5'
+        style={{ width: 'calc(3 * 134px)', justifyContent: 'space-between' }}
+      >
+        {/* 이전 버튼 */}
+        <button onClick={handlePrevClick}>
+          <img src='./icon/angle_left_white.png' alt='이전' className='w-6' />
+        </button>
+
+        {/* 케러셀 컴포넌트 */}
+        {isCameraOn && (
+          <Carousel
+            items={
+              currentPageIdealPersons?.map((p) => ({
+                idealPersonImageUrl: p.idealPersonImageUrl, // 이전에는 idealPersonImage였습니다.
+                idealPersonNickname: p.idealPersonNickname,
+              })) || []
+            }
+            selectedImage={selectedIdealPersonImage}
+            onSelectImage={selectIdealPersonImage}
+          />
+        )}
+
+        {/* 다음 버튼 */}
+        <button onClick={handleNextClick}>
+          <img src='./icon/angle_left_white.png' alt='다음' className='w-6' style={{ transform: 'scaleX(-1)' }} />
+        </button>
+      </div>
+
+      {isCameraOn && (
+        <div className='flex flex-row justify-center items-center space-x-12'>
+          {/* 돌아가기 버튼 */}
+          <button
+            onClick={handleGoBack}
+            disabled={!isPictureTaken}
+            className={`px-2 py-2 rounded-lg transition-colors duration-300 flex items-center space-x-2 ${
+              isPictureTaken ? 'bg-[#AB42CF] text-white' : 'bg-gray-400 text-gray-600'
+            }`}
+          >
+            <img
+              src={isPictureTaken ? './icon/camera_back_white.png' : './icon/camera_back_gray.png'}
+              alt='돌아가기'
+              className='w-5 h-5'
+            />
+            <span>다시찍기</span>
           </button>
-
-          {/* 케러셀 컴포넌트 */}
-          {isCameraOn && (
-            <Carousel
-                items={currentPageIdealPersons?.map((p) => ({
-                  idealPersonImageUrl: p.idealPersonImageUrl, // 이전에는 idealPersonImage였습니다.
-                  idealPersonNickname: p.idealPersonNickname
-                })) || []}
-                selectedImage={selectedIdealPersonImage}
-                onSelectImage={selectIdealPersonImage}
-              />
+          {/* 촬영 버튼 */}
+          {!isPictureTaken ? (
+            <img
+              src={isHovering ? './icon/camera_start2.png' : './icon/camera_start.png'}
+              alt='사진 촬영'
+              onClick={takePicture}
+              onMouseEnter={() => setIsHovering(true)}
+              onMouseLeave={() => setIsHovering(false)}
+              style={cameraImgStyle}
+            />
+          ) : (
+            <div className='success-icon'>
+              <div className='success-icon__tip'></div>
+              <div className='success-icon__long'></div>
+            </div>
           )}
-
-          {/* 다음 버튼 */}
-          <button onClick={handleNextClick}>
-            <img src="./icon/angle_left_white.png" alt="다음" className='w-6' style={{ transform: 'scaleX(-1)' }} />
+          {/* 저장하기 버튼 */}
+          <button
+            onClick={handleSavePicture}
+            disabled={!isPictureTaken}
+            className={`px-2 py-2 rounded-lg transition-colors duration-300 flex items-center space-x-2 ${
+              isPictureTaken ? 'bg-[#AB42CF] text-white' : 'bg-gray-400 text-gray-600'
+            }`}
+          >
+            <img
+              src={isPictureTaken ? './icon/camera_save_white.png' : './icon/camera_save_gray.png'}
+              alt='저장하기'
+              className='w-5 h-5'
+            />
+            <span>저장하기</span>
           </button>
         </div>
-
-        {isCameraOn && (
-          <div className='flex flex-row justify-center items-center space-x-12'>
-            {/* 돌아가기 버튼 */}
-            <button
-              onClick={handleGoBack}
-              disabled={!isPictureTaken}
-              className={`px-2 py-2 rounded-lg transition-colors duration-300 flex items-center space-x-2 ${
-                isPictureTaken ? 'bg-[#AB42CF] text-white' : 'bg-gray-400 text-gray-600'
-              }`}
-            >
-              <img
-                  src={isPictureTaken ? './icon/camera_back_white.png' : './icon/camera_back_gray.png'}
-                  alt="돌아가기"
-                  className="w-5 h-5"
-                />
-                <span>다시찍기</span>
-            </button>
-            {/* 촬영 버튼 */}
-            {!isPictureTaken ? (
-              <img
-                src={isHovering ? './icon/camera_start2.png' : './icon/camera_start.png'}
-                alt='사진 촬영'
-                onClick={takePicture}
-                onMouseEnter={() => setIsHovering(true)}
-                onMouseLeave={() => setIsHovering(false)}
-                style={cameraImgStyle}
-              />
-            ) : (
-              <div className="success-icon">
-                <div className="success-icon__tip"></div>
-                <div className="success-icon__long"></div>
-              </div>        
-            )}
-            {/* 저장하기 버튼 */}
-              <button
-                onClick={handleSavePicture}
-                disabled={!isPictureTaken}
-                className={`px-2 py-2 rounded-lg transition-colors duration-300 flex items-center space-x-2 ${
-                  isPictureTaken ? 'bg-[#AB42CF] text-white' : 'bg-gray-400 text-gray-600'
-                }`}
-              >
-                <img
-                  src={isPictureTaken ? './icon/camera_save_white.png' : './icon/camera_save_gray.png'}
-                  alt="저장하기"
-                  className="w-5 h-5"
-                />
-                <span>저장하기</span>
-              </button>
-          </div>
-          )}
-        {/* 카메라 시작 버튼 (카메라가 꺼져있을 때만 표시) */}
-        {!isCameraOn && (
-          <button onClick={startCamera}>카메라 시작</button>
-        )}
+      )}
+      {/* 카메라 시작 버튼 (카메라가 꺼져있을 때만 표시) */}
+      {!isCameraOn && <button onClick={startCamera}>카메라 시작</button>}
     </div>
   );
 };
