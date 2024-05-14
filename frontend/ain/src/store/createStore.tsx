0ac81a7c16297229c947ef69cb677ce82f7e2981
백천주 <dieswing@naver.com>
2024-05-14 18:29:01 +0900

    feat: Add temp localstorage connection to confirm data in create store

--- frontend/ain/src/store/createStore.tsx
+++ frontend/ain/src/store/createStore.tsx
@@ -1,21 +1,30 @@
 import { create } from 'zustand';
+import { createJSONStorage, persist } from 'zustand/middleware';
 
 interface StoreState {
-     mergeInput: string,
-     genderInput: string|null,
-     mbti: string,
-     imageUrl: string,
-     characterName: string,
-     imageFile: Blob|null
-  }
-  
-const useCreateStore = create<StoreState>(set => ({
-    mergeInput: '',
-    genderInput: null,
-    mbti: '',
-    imageUrl: '',
-    characterName: '',
-    imageFile: null
-}))
-  
+  mergeInput: string;
+  genderInput: string;
+  mbti: string;
+  imageUrl: string;
+  characterName: string;
+  imageFile: Blob | null;
+}
+
+const useCreateStore = create(
+  persist<StoreState>(
+    (set) => ({
+      mergeInput: '',
+      genderInput: '',
+      mbti: '',
+      imageUrl: '',
+      characterName: '',
+      imageFile: null,
+    }),
+    {
+      name: 'create',
+      storage: createJSONStorage(() => localStorage),
+    }
+  )
+);
+
 export default useCreateStore;
