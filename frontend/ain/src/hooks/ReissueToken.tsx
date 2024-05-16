7b5db2856bdddcf2785254a6de7e3a181f8bf15a
백천주 <dieswing@naver.com>
2024-05-16 18:09:52 +0900

    feat: Add reissue token component

---
+++ frontend/ain/src/hooks/ReissueToken.tsx
@@ -0,0 +1,46 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+
+import useUserStore from '@/store/userStore';
+
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
+
+export default function Page() {
+  const { accessToken, refreshToken } = useUserStore();
+  const [newAccessToken, setNewAccessToken] = useState();
+  const [newRefreshToken, setNewRefreshToken] = useState();
+
+  const { setAccessToken } = useUserStore();
+
+  useEffect(() => {
+    const reissueTokens = async () => {
+      try {
+        const res = await fetch(`${API_URL}/auth/reissue`, {
+          method: 'POST',
+          credentials: 'include',
+        });
+        if (res.ok) {
+          const result = await res.json();
+          console.log('결과', result);
+          const tempAccessToken = res.headers.get('Authorization');
+          if (tempAccessToken !== undefined && tempAccessToken !== null) {
+            setAccessToken(tempAccessToken);
+          }
+        } else {
+          alert('실패');
+          console.log(res.status);
+          console.log('대실패');
+        }
+      } catch (error) {
+        alert('실패2');
+        console.log(error);
+        return;
+      }
+    };
+
+    reissueTokens();
+  }, []);
+
+  return null;
+}
