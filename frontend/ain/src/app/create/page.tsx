1e762f6ebf1982e9d1d6ac2b1fd54142ed4997b1
백천주 <dieswing@naver.com>
2024-05-16 11:26:18 +0900

    feat: Modify codes of create, loading, result page

--- frontend/ain/src/app/create/page.tsx
+++ frontend/ain/src/app/create/page.tsx
@@ -1,133 +1,174 @@
-'use client'
+'use client';
 
-import useCreateStore from "@/store/createStore"
-import { useState } from "react"
-import Link from "next/link"
+import useCreateStore from '@/store/createStore';
+import { useState } from 'react';
+import Link from 'next/link';
 
 export default function CreatePage() {
+  const [currentNum, setCurrentNum] = useState<number>(1);
 
-  const [currentNum, setCurrentNum] = useState<number>(1)
-
-  const [genderInput, setGenderInput] = useState<string|null>(null)
-  const [faceInput, setFaceInput] = useState<string>('')
-  const [shapeInput, setShapeInput] = useState<string>('')
-  const [ectInput, setEctInput] = useState<string>('')
+  const [genderInput, setGenderInput] = useState<string | null>(null);
+  const [faceInput, setFaceInput] = useState<string>('');
+  const [shapeInput, setShapeInput] = useState<string>('');
+  const [ectInput, setEctInput] = useState<string>('');
 
   const submitInput = () => {
     if (genderInput !== null) {
-      useCreateStore.setState(state => ({ mergeInput: faceInput + shapeInput + ectInput, genderInput: genderInput}))
+      useCreateStore.setState((state) => ({ mergeInput: faceInput + shapeInput + ectInput, genderInput: genderInput }));
+    } else {
+      alert('성별을 선택해 주세요');
+      setCurrentNum(1);
     }
-    else {
-      alert('성별을 선택해 주세요')
-      setCurrentNum(1)
-    }
-  }
+  };
 
-  return <div className="mt-[65px] mb-[68px] w-full h-full flex flex-col">
-    {/* 현재 인덱스 표시 */}
-    <div className="w-full h-[15%] flex items-center justify-center">
-      <div  className={`w-3 h-3 mx-2 ${currentNum === 1 ? 'bg-white' : 'bg-[#D9D9D9]'} rounded-full shadow-md`}></div>
-      <div  className={`w-3 h-3 mx-2 ${currentNum === 2 ? 'bg-white' : 'bg-[#D9D9D9]'} rounded-full shadow-md`}></div>
-      <div  className={`w-3 h-3 mx-2 ${currentNum === 3 ? 'bg-white' : 'bg-[#D9D9D9]'} rounded-full shadow-md`}></div>
-      <div  className={`w-3 h-3 mx-2 ${currentNum === 4 ? 'bg-white' : 'bg-[#D9D9D9]'} rounded-full shadow-md`}></div>
-      <div  className={`w-3 h-3 mx-2 ${currentNum === 5 ? 'bg-white' : 'bg-[#D9D9D9]'} rounded-full shadow-md`}></div>
-    </div>
+  return (
+    <div className='mt-[65px] mb-[68px] w-full h-full flex flex-col'>
+      {/* 현재 인덱스 표시 */}
+      <div className='w-full h-[15%] flex items-center justify-center'>
+        <div className={`w-3 h-3 mx-2 ${currentNum === 1 ? 'bg-white' : 'bg-[#D9D9D9]'} rounded-full shadow-md`}></div>
+        <div className={`w-3 h-3 mx-2 ${currentNum === 2 ? 'bg-white' : 'bg-[#D9D9D9]'} rounded-full shadow-md`}></div>
+        <div className={`w-3 h-3 mx-2 ${currentNum === 3 ? 'bg-white' : 'bg-[#D9D9D9]'} rounded-full shadow-md`}></div>
+        <div className={`w-3 h-3 mx-2 ${currentNum === 4 ? 'bg-white' : 'bg-[#D9D9D9]'} rounded-full shadow-md`}></div>
+        <div className={`w-3 h-3 mx-2 ${currentNum === 5 ? 'bg-white' : 'bg-[#D9D9D9]'} rounded-full shadow-md`}></div>
+      </div>
 
-    {/* 입력칸 영역 */}
-    <div className="w-full h-[85%] flex flex-row items-start justify-center">
-      
-      {currentNum !== 5 ? (
-      <div className="w-[70%] h-[90%] bg-[#F0D5FA] rounded-xl shadow-lg  flex flex-col items-center">
-        <div className="w-[80%] h-6 mt-[10%]">
-          {currentNum != 1 &&(
-          <button onClick={(event) => setCurrentNum(currentNum -1)}>
-            <img src="./icon/angle_left_purple.png" className="w-[10px]" />            
-          </button>)}
-        </div>
-
-        {/* 질문 */}
-        <div className="w-[80%] h-[15%] flex flex-col justify-start text-center text-[17px] text-bold text-black">
-        {currentNum === 1 && (
-          <p>아인의 성별은 <br />무엇인가요?</p>
-        )}
-        {currentNum === 2 && (
-          <p>당신의 아인은 <br />어떤 이목구비를 가졌나요?</p>
-        )}
-        {currentNum === 3 && (
-          <p>어떤 얼굴형과 <br />헤어스타일을 가졌나요?</p>
-        )}
-        {currentNum === 4 && (
-          <p>기타 생김새를 <br /> 입력해주세요</p>
-        )}
-        </div>   
-
-         {/* 이상형 성별 입력 받기 */}
-        {currentNum == 1 ? (
-        <div className="w-[80%] h-[60%] flex justify-center items-center bg-white rounded-xl px-4">
-          <button  className={`flex flex-col items-center py-2 border-r-2 border-dashed rounded-md 
-          ${genderInput === 'MALE' ? 'bg-[#C5EEFF]' : 'hover:bg-[#C5EEFF]'}`}
-          onClick={(event)=> {setGenderInput('MALE'), setCurrentNum(2)}}>
-            <img src="./icon/boy.png" className="w-[80%]"/>
-            <p className="mt-6">남성</p>
-          </button>
-          <button  className={`flex flex-col items-center py-2 rounded-md 
-          ${genderInput === 'FEMALE' ? 'bg-[#FFD2F2]' : 'hover:bg-[#FFD2F2]'}`}
-          onClick={(event)=> {setGenderInput('FEMALE'), setCurrentNum(2)}}>
-            <img src="./icon/girl.png" className="w-[80%]"/>   
-            <p className="mt-6">여성</p>         
-          </button>
-        </div>)
-
-        // 이상형 생김새 입력 받기
-        : (
-        <div className="w-[80%] h-[50%] flex justify-center items-center bg-white rounded-xl">
-        {currentNum === 2 && (
-          <textarea className="w-[90%] text-sm outline-none resize-none text-center" rows={10}
-          value={faceInput} onChange={(event)=> setFaceInput(event.target.value)} placeholder='이목구비 입력 예시'/>
-        )}
-        {currentNum === 3 && (
-          <textarea className="w-[90%] text-sm outline-none resize-none text-center" rows={10}
-          value={shapeInput} onChange={(event)=> setShapeInput(event.target.value)} placeholder='얼굴형, 헤어스타일 입력 예시'/>
-        )}
-        {currentNum === 4 && (
-          <textarea className="w-[90%] text-sm outline-none resize-none text-center" rows={10}
-          value={ectInput} onChange={(event)=> setEctInput(event.target.value)} placeholder='기타 생김새 입력 예시'/>
-        )}
-        </div>)}
-
-        {/* 입력완료, 잘 모르겠어요 버튼 */}
-        {currentNum !== 1 && (
-        <div className="w-[80%] h-[25%] flex flex-col items-center justify-center">
-          <button className="w-[150px] h-[50px] bg-[#C776E3] rounded-2xl shadow-md text-center text-white text-lg hover:bg-[#AB42CF]"
-          onClick={(event) => setCurrentNum(currentNum + 1)}>
-            <p>입력 완료</p>
-          </button>
-          <button className="text-xs mt-4 text-[#5F0F7A]" 
-          onClick={(event) => setCurrentNum(currentNum + 1)}>잘 모르겠어요..</button>
-        </div>)}
-      </div>)
-
-      // currentNum이 5일때
-      : (
-        <div className="w-[70%] h-full flex flex-col justify-start items-center">
-          <img src="./gif/question.gif" className="w-[70%]"/>
-          <div className="w-[90%] h-[220px] bg-[#F0D5FA] rounded-2xl flex flex-col items-center justify-evenly shadow-xl">
-            <div className="w-[80%] h-6 flex flex-col justify-end">
-              <button onClick={(event) => setCurrentNum(currentNum -1)}>
-                <img src="./icon/angle_left_purple.png" className="w-[10px]" />            
-              </button>
+      {/* 입력칸 영역 */}
+      <div className='w-full h-[85%] flex flex-row items-start justify-center'>
+        {currentNum !== 5 ? (
+          <div className='w-[70%] h-[90%] bg-[#F0D5FA] rounded-xl shadow-lg  flex flex-col items-center'>
+            <div className='w-[80%] h-6 mt-[10%]'>
+              {currentNum != 1 && (
+                <button onClick={(event) => setCurrentNum(currentNum - 1)}>
+                  <img src='./icon/angle_left_purple.png' className='w-[10px]' />
+                </button>
+              )}
             </div>
-            <p className=" w-full text-lg text-center text-black">아인이 당신을 <br />기다리고 있어요!</p>
-            <Link href='/loading' className="w-full flex justify-center">
-            <button className="w-[70%] h-14 mb-2 bg-[#AB42CF] rounded-xl text-center text-white shadow-md"
-            onClick={submitInput}>만나러 가기</button>
-            </Link>                  
+
+            {/* 질문 */}
+            <div className='w-[80%] h-[15%] flex flex-col justify-start text-center text-[17px] text-bold text-black'>
+              {currentNum === 1 && (
+                <p>
+                  아인의 성별은 <br />
+                  무엇인가요?
+                </p>
+              )}
+              {currentNum === 2 && (
+                <p>
+                  당신의 아인은 <br />
+                  어떤 이목구비를 가졌나요?
+                </p>
+              )}
+              {currentNum === 3 && (
+                <p>
+                  어떤 얼굴형과 <br />
+                  헤어스타일을 가졌나요?
+                </p>
+              )}
+              {currentNum === 4 && (
+                <p>
+                  기타 생김새를 <br /> 입력해주세요
+                </p>
+              )}
+            </div>
+
+            {/* 이상형 성별 입력 받기 */}
+            {currentNum == 1 ? (
+              <div className='w-[80%] h-[60%] flex justify-center items-center bg-white rounded-xl px-4'>
+                <button
+                  className={`flex flex-col items-center py-2 border-r-2 border-dashed rounded-md 
+          ${genderInput === 'MALE' ? 'bg-[#C5EEFF]' : 'hover:bg-[#C5EEFF]'}`}
+                  onClick={(event) => {
+                    setGenderInput('MALE'), setCurrentNum(2);
+                  }}
+                >
+                  <img src='./icon/boy.png' className='w-[80%]' />
+                  <p className='mt-6'>남성</p>
+                </button>
+                <button
+                  className={`flex flex-col items-center py-2 rounded-md 
+          ${genderInput === 'FEMALE' ? 'bg-[#FFD2F2]' : 'hover:bg-[#FFD2F2]'}`}
+                  onClick={(event) => {
+                    setGenderInput('FEMALE'), setCurrentNum(2);
+                  }}
+                >
+                  <img src='./icon/girl.png' className='w-[80%]' />
+                  <p className='mt-6'>여성</p>
+                </button>
+              </div>
+            ) : (
+              // 이상형 생김새 입력 받기
+              <div className='w-[80%] h-[50%] flex justify-center items-center bg-white rounded-xl'>
+                {currentNum === 2 && (
+                  <textarea
+                    className='w-[90%] text-sm outline-none resize-none text-center'
+                    rows={10}
+                    value={faceInput}
+                    onChange={(event) => setFaceInput(event.target.value)}
+                    placeholder='이목구비 입력 예시'
+                  />
+                )}
+                {currentNum === 3 && (
+                  <textarea
+                    className='w-[90%] text-sm outline-none resize-none text-center'
+                    rows={10}
+                    value={shapeInput}
+                    onChange={(event) => setShapeInput(event.target.value)}
+                    placeholder='얼굴형, 헤어스타일 입력 예시'
+                  />
+                )}
+                {currentNum === 4 && (
+                  <textarea
+                    className='w-[90%] text-sm outline-none resize-none text-center'
+                    rows={10}
+                    value={ectInput}
+                    onChange={(event) => setEctInput(event.target.value)}
+                    placeholder='기타 생김새 입력 예시'
+                  />
+                )}
+              </div>
+            )}
+
+            {/* 입력완료, 잘 모르겠어요 버튼 */}
+            {currentNum !== 1 && (
+              <div className='w-[80%] h-[25%] flex flex-col items-center justify-center'>
+                <button
+                  className='w-[150px] h-[50px] bg-[#C776E3] rounded-2xl shadow-md text-center text-white text-lg hover:bg-[#AB42CF]'
+                  onClick={(event) => setCurrentNum(currentNum + 1)}
+                >
+                  <p>입력 완료</p>
+                </button>
+                <button className='text-xs mt-4 text-[#5F0F7A]' onClick={(event) => setCurrentNum(currentNum + 1)}>
+                  잘 모르겠어요..
+                </button>
+              </div>
+            )}
           </div>
-        </div>
-      )}
-      
+        ) : (
+          // currentNum이 5일때
+          <div className='w-[70%] h-full flex flex-col justify-start items-center'>
+            <img src='./gif/question.gif' className='w-[70%]' />
+            <div className='w-[90%] h-[220px] bg-[#F0D5FA] rounded-2xl flex flex-col items-center justify-evenly shadow-xl'>
+              <div className='w-[80%] h-6 flex flex-col justify-end'>
+                <button onClick={(event) => setCurrentNum(currentNum - 1)}>
+                  <img src='./icon/angle_left_purple.png' className='w-[10px]' />
+                </button>
+              </div>
+              <p className=' w-full text-lg text-center text-black'>
+                아인이 당신을 <br />
+                기다리고 있어요!
+              </p>
+              <Link href='/loading' className='w-full flex justify-center'>
+                <button
+                  className='w-[70%] h-14 mb-2 bg-[#AB42CF] rounded-xl text-center text-white shadow-md'
+                  onClick={submitInput}
+                >
+                  만나러 가기
+                </button>
+              </Link>
+            </div>
+          </div>
+        )}
+      </div>
     </div>
-  </div>
+  );
 }
-
-
--- frontend/ain/src/app/loading/page.tsx
+++ frontend/ain/src/app/loading/page.tsx
@@ -1,23 +1,14 @@
-"use client";
+'use client';
 
-import useCreateStore from "@/store/createStore";
-import { useState, useEffect } from "react";
-import { useRouter } from "next/navigation";
+import useCreateStore from '@/store/createStore';
+import { useState, useEffect } from 'react';
+import { useRouter } from 'next/navigation';
 
 export default function LoadingPage() {
   const router = useRouter();
 
   const { mergeInput, genderInput } = useCreateStore();
-  const [mbtiValue, setMbtiValue] = useState<string>("");
-
-  const blobToBase64 = (blob: Blob) => {
-    return new Promise((resolve, reject) => {
-      const reader = new FileReader();
-      reader.onload = () => resolve(reader.result);
-      reader.onerror = (error) => reject(error);
-      reader.readAsDataURL(blob);
-    });
-  };
+  const [mbtiValue, setMbtiValue] = useState<string>('');
 
   const fetchCharacterImage = async () => {
     const requestBody = {
@@ -26,71 +17,31 @@ export default function LoadingPage() {
     };
 
     try {
-      await fetch(
-        `${process.env.NEXT_PUBLIC_FAST_API_URL}/ideal-people/images`,
-        {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(requestBody),
-        }
-      )
-        .then(
-          (response: {
-            blob(): any;
-            headers: { get: (arg0: string) => any };
-          }) => {
-            // 응답 헤더에서 'Mbti' 값을 추출
-            const newMbti: string = response.headers.get("mbti");
-            useCreateStore.setState((state) => ({ mbti: newMbti }));
-            return response.blob();
-          }
-        )
-        .then(async (blob) => {
-          // const base64 = await blobToBase64(blob);
+      await fetch(`${process.env.NEXT_PUBLIC_FAST_API_URL}/ideal-people/images`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(requestBody),
+      })
+        .then((response: { blob(): any; headers: { get: (arg0: string) => any } }) => {
+          // 응답 헤더에서 'Mbti' 값을 추출
+          const newMbti: string = response.headers.get('mbti');
+          useCreateStore.setState((state) => ({ mbti: newMbti }));
+          return response.blob();
+        })
+        .then((blob) => {
           // 받은 파일(blob)을 처리하는 로직을 작성합니다.
           const newUrl: string = URL.createObjectURL(blob); // blob 객체를 가리키는 URL을 생성
-          const arrayBuffer = await blob.arrayBuffer();
-          useCreateStore.setState((state) => ({
-            imageUrl: newUrl,
-            imageFile: arrayBuffer,
-          }));
+          useCreateStore.setState((state) => ({ imageUrl: newUrl, imageFile: blob }));
           // const image = document.createElement('img'); //새로운 <img> HTML 요소를 생성
           // image.src = url; //생성된 이미지 요소(image)의 소스(src) 속성에, 2단계에서 생성된 blob URL을 할당
           // document.body.appendChild(image);//이미지 요소를 문서의 body에 추가
         });
     } catch (error) {
-      console.error("API request failed: ", error);
+      console.error('API request failed: ', error);
     }
   };
-
-  // const fetchCharacterName = async () => {
-  //   const requestBody = {
-  //     idealPersonGender: genderInput
-  //   };
-
-  //   try {
-  //     const response = await fetch('https://myain.co.kr/api/ideal-people/names', {
-  //       // const response = await fetch('https://bad7e4c4-8676-4672-86a2-212cf9b3de90.mock.pstmn.io/api/ideal-people/names', {
-  //       method: 'POST',
-  //       headers: {
-  //         'Content-Type': 'application/json',
-  //       },
-  //       body: JSON.stringify(requestBody),
-  //     })
-  //     return response.json()
-
-  //     .then((response): any  => {
-
-  //       const newCharacterName : string = response.data.idealPersonName
-  //       useCreateStore.setState(state => ({ characterName: newCharacterName }))
-
-  //     })
-  //   } catch (error) {
-  //     console.error('API request failed: ', error);
-  //   }
-  // }
 
   const fetchCharacterName = async () => {
     // 쿼리 파라미터 문자열 생성
@@ -99,52 +50,40 @@ export default function LoadingPage() {
     }).toString();
 
     try {
-      const response = await fetch(
-        `${process.env.NEXT_PUBLIC_API_URL}/ideal-people/names?${queryParams}`,
-        {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-          },
-        }
-      );
+      const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/ideal-people/names?${queryParams}`, {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
 
       const data = await response.json();
       const newCharacterName: string = data.data.idealPersonName;
       useCreateStore.setState((state) => ({ characterName: newCharacterName }));
     } catch (error) {
-      console.error("API request failed: ", error);
+      console.error('API request failed: ', error);
     }
   };
 
   useEffect(() => {
     const fetchData = async () => {
       await Promise.all([fetchCharacterImage(), fetchCharacterName()]);
-      router.push("/result");
+      router.push('/result');
     };
 
     fetchData();
   }, []);
 
   return (
-    <div className="relative mt-[65px] mb-[68px] w-full h-full flex flex-col justify-center items-center">
-      <img
-        src="./gif/loading_circle.gif"
-        className="w-[55%] rounded-full shadow-xl"
-      />
-      <p className="text-center text-xl text-white mt-10 leading-relaxed">
+    <div className='relative mt-[65px] mb-[68px] w-full h-full flex flex-col justify-center items-center'>
+      <img src='./gif/loading_circle.gif' className='w-[55%] rounded-full shadow-xl' />
+      <p className='text-center text-xl text-white mt-10 leading-relaxed'>
         조금만 기다려주시면 <br />
-        아인과의 <span className="text-[#FF7FCE]">아름다운 만남</span>이 <br />
+        아인과의 <span className='text-[#FF7FCE]'>아름다운 만남</span>이 <br />
         시작될 거예요
       </p>
-      <img
-        src="./background/loading_top.png"
-        className="absolute top-0 right-0 w-1/3"
-      />
-      <img
-        src="./background/loading_bottom.png"
-        className="absolute bottom-0 left-0 w-[60%]"
-      />
+      <img src='./background/loading_top.png' className='absolute top-0 right-0 w-1/3' />
+      <img src='./background/loading_bottom.png' className='absolute bottom-0 left-0 w-[60%]' />
     </div>
   );
 }
