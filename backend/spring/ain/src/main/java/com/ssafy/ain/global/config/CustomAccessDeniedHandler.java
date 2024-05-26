3c8117d307192587d0f0f368bda342b9eb503a2e
ykim-yeji <68416691+ykim-yeji@users.noreply.github.com>
2024-05-26 18:41:24 +0900

    refactor: Filter 단에서 발생하는 예외 처리 코드 중복 해결

--- backend/spring/ain/src/main/java/com/ssafy/ain/global/config/CustomAccessDeniedHandler.java
+++ backend/spring/ain/src/main/java/com/ssafy/ain/global/config/CustomAccessDeniedHandler.java
@@ -1,40 +1,26 @@
 package com.ssafy.ain.global.config;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.ssafy.ain.global.constant.ErrorCode;
 import com.ssafy.ain.member.service.AuthService;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
+
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.security.web.access.AccessDeniedHandler;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 @Component
 @RequiredArgsConstructor
 public class CustomAccessDeniedHandler implements AccessDeniedHandler {
 
-    private final ObjectMapper objectMapper;
     private final AuthService authService;
 
     @Override
     public void handle(HttpServletRequest request, HttpServletResponse response,
                        AccessDeniedException accessDeniedException) throws IOException, ServletException {
-        Object exception = request.getAttribute("exception");
-        Map<String, Object> errorResponse = null;
-        if (exception instanceof ErrorCode) {
-            errorResponse = authService.getErrorResponse((ErrorCode) exception);
-        } else {
-            errorResponse = authService.getErrorResponse(accessDeniedException);
-        }
-        response.setContentType("application/json");
-        response.setCharacterEncoding("UTF-8");
-        response.getWriter().write(objectMapper.writeValueAsString(errorResponse));
+        authService.addErrorInResponse(request, response, accessDeniedException);
     }
 }
--- backend/spring/ain/src/main/java/com/ssafy/ain/member/service/AuthService.java
+++ backend/spring/ain/src/main/java/com/ssafy/ain/member/service/AuthService.java
@@ -1,11 +1,12 @@
 package com.ssafy.ain.member.service;
 
-import com.ssafy.ain.global.constant.ErrorCode;
+import java.io.IOException;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+
 import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
-import java.util.Map;
 
 public interface AuthService {
 
@@ -15,6 +16,6 @@ public interface AuthService {
 	void isTokenExpired(String token, String category);
 	void equalTokenCategory(String token, String categoryForCheck);
 	void existRefreshToken(String refreshToken);
-	Map<String, Object> getErrorResponse(ErrorCode errorCode);
-	Map<String, Object> getErrorResponse(RuntimeException runtimeException);
+	void addErrorInResponse(HttpServletRequest request, HttpServletResponse response,
+		RuntimeException runtimeException) throws IOException;
 }
